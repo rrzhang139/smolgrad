c83c00060363af4f79d33fb7e36b692e257c1981
Richard Zhang <rzhang139@gmail.com>
2024-06-21 17:09:00 -0700

    Implement matrix multiplication (matmul) with autograd support
    - Add matmul operation to Tensor class
    - Implement broadcasting logic for batch dimensions
    - Handle 1D tensor edge cases in matmul
    - Add gradient computation for matmul in backward pass
    - Discuss and clarify complex gradient reshaping and summing operations
    - Explain the mathematical basis for gradient computation in matrix multiplication
    - Improve understanding of broadcasting and "un-broadcasting" in gradient calculation

--- core/tensor.py
+++ core/tensor.py
@@ -26,7 +26,7 @@ class Tensor:
             # Backpropogating and updating gradients using chain rule
             v._backward()
 
-    def __add__(self, other):
+    def __add__(self, other) -> "Tensor":
         other = other if isinstance(other, Tensor) else Tensor(other)
         result = Tensor(self.data + other.data,
                         requires_grad=self.requires_grad or other.requires_grad)
@@ -43,7 +43,7 @@ class Tensor:
 
         return result
 
-    def __mul__(self, other):
+    def __mul__(self, other) -> "Tensor":
         other = other if isinstance(other, Tensor) else Tensor(other)
         result = Tensor(self.data * other.data,
                         requires_grad=self.requires_grad or other.requires_grad)
@@ -60,6 +60,106 @@ class Tensor:
 
         return result
 
-    def sum(self):
-        # TODO: Implement sum operation
-        pass
+    def __matmul__(self, other) -> "Tensor":
+        result = Tensor(self.data @ other.data,
+                        requires_grad=self.requires_grad or other.requires_grad)
+
+        if result.requires_grad:
+            le_axis = (0,) if self.data.ndim == 1 else ()
+            re_axis = (-1,) if other.data.ndim == 1 else ()
+
+            result_axis = le_axis + re_axis
+
+            # When performing matrix multiplication, there could be dimensions before it.
+            # Important to try leave the dimensions of the matrix mul for now, and just broadcast the outer dims
+            # inputting dims in tuple
+            l_indices, r_indices = broadcast(
+                self.data.shape[:-2], other.data.shape[:-2])
+
+            def _backward():
+                if self.requires_grad:
+                    self.grad = np.reshape(
+                        np.sum(
+                            np.expand_dims(result.grad, axis=result_axis) @
+                            np.expand_dims(
+                                other.data, axis=re_axis).swapaxes(-1, -2),
+                            axis=l_indices
+                        ),
+                        newshape=self.grad.shape
+                    )
+                if other.requires_grad:
+                    other.grad = np.reshape(
+                        np.sum(
+                            np.expand_dims(
+                                self.data, axis=le_axis).swapaxes(-1, -2) @
+                            np.expand_dims(result.grad, axis=result_axis),
+                            axis=r_indices
+                        ),
+                        newshape=other.grad.shape
+                    )
+
+            result._backward = _backward
+            result._prev = {self, other}
+
+        return result
+
+    def sum(self) -> "Tensor":
+        result = Tensor(np.sum(self.data), requires_grad=self.requires_grad)
+
+        if result.requires_grad:
+            def _backward():
+                if self.requires_grad:
+                    self.grad += np.ones_like(self.data) * result.grad
+
+            result._backward = _backward
+            result._prev = {self}
+
+        return result
+
+    def __neg__(self):
+        return self * -1
+
+    def __sub__(self, other: "Tensor"):
+        return self + (-other)
+
+    def __rsub__(self, other: "Tensor"):    # other - self
+        return -self + other
+
+    def __radd__(self, other: "Tensor"):  # other + self
+        return self + other
+
+    def __rmul__(self, other: "Tensor"):  # other * self
+        return self * other
+
+    def __truediv__(self, other: "Tensor"):
+        return self * (other ** -1)
+
+    def __rtruediv__(self, other: "Tensor"):    # other / self
+        return (self ** -1) * other
+
+    def __repr__(self) -> str:
+        if self.requires_grad:
+            return f"Tensor({self.data}, requires_grad={self.requires_grad})"
+
+        return f"Tensor({self.data})"
+
+
+def broadcast(left: tuple, right: tuple) -> (tuple, tuple):
+    lsize = len(left)
+    rsize = len(right)
+
+    maxsize = max(lsize, rsize)
+
+    newleft = (1,) * (maxsize - lsize) + left
+    newright = (1,) * (maxsize - rsize) + right
+
+    assert len(newleft) == len(newright)
+
+    # now we can collect the indices where right or left needs to broadcast to match the dim
+    l_indices, r_indicies = [], []
+    for i in range(len(newleft)):
+        if newleft[i] > newright[i]:
+            r_indicies.append(i)
+        else:
+            l_indices.append(i)
+    return tuple(l_indices), tuple(r_indicies)
--- test_tensor.py
+++ test_tensor.py
@@ -28,9 +28,32 @@ class TestTensor(unittest.TestCase):
         self.assertTrue(z.requires_grad)
 
         z.backward()
-        print(x.grad)
         self.assertTrue(np.array_equal(x.grad, np.array([4, 5, 6])))
         self.assertTrue(np.array_equal(y.grad, np.array([1, 2, 3])))
+
+    def test_sum(self):
+        x = Tensor([1, 2, 3], requires_grad=True)
+        z = x.sum()
+
+        self.assertEqual(z.data, 6)
+        self.assertTrue(z.requires_grad)
+
+        z.backward()
+
+        self.assertTrue(np.array_equal(x.grad, np.array([1, 1, 1])))
+
+    def test_matmul(self):
+        x = Tensor([1, 2, 3], requires_grad=True)
+        y = Tensor([4, 5, 6], requires_grad=True)
+        z = x @ y
+
+        self.assertEqual(z.data, 32)
+        self.assertTrue(z.requires_grad)
+
+        z.backward()
+
+        self.assertTrue(np.array_equal(x.grad, y.data))
+        self.assertTrue(np.array_equal(y.grad, x.data))
 
 
 if __name__ == '__main__':
